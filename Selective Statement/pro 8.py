22e25180e86c824d04cf2ed10558d91290a9671e
Selvaganesh.S <130886211+Selvaganesh2244@users.noreply.github.com>
2023-05-24 21:27:07 +0530

    Update pro 8.py

--- Selective Statement/pro 8.py
+++ Selective Statement/pro 8.py
@@ -10,15 +10,3 @@ elif(age>=30 and age<40 and g=='M'):
     print("your total wage will be: ",nod*800)
 elif(age>=30 and age<40 and g=='F'):
     print("your total wage will be: ",nod*800)
-name =input("Enter your name: ")
-age = int(input("Enter your age: "))
-g = input("Enter your Gender(M/F): ")
-nod =int(input("Enter the number of days you're ready to work: "))
-if(age>=18 and age<30 and g=='M'):
-    print("your total wage will be: ",nod*700)
-elif(age>=18 and g=='F'):
-    print("your total wage will be: ",nod*750)
-elif(age>=30 and age<40 and g=='M'):
-    print("your total wage will be: ",nod*800)
-elif(age>=30 and age<40 and g=='F'):
-    print("your total wage will be: ",nod*800)

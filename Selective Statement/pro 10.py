854aaf373c25b68394009f7722e08d9830e7deb3
Selvaganesh.S <130886211+Selvaganesh2244@users.noreply.github.com>
2023-05-25 11:23:25 +0530

    leap year or not

---
+++ Selective Statement/pro 10.py
@@ -0,0 +1,9 @@
+a=int(input("enter the year:"))
+if(a%400==0):
+     print("it is a leap year")
+elif(a%100==0):
+     ptint("it is not leap year")
+elif(a%4==0):
+     print("it is a leap year")
+else:
+     print("it is not leap year")

74b5406991b8c2c0dabf4ae819993a8121e1f785
Selvaganesh.S <130886211+Selvaganesh2244@users.noreply.github.com>
2023-07-05 23:04:51 +0530

    Add files via upload

---
+++ Tuple programs/pro 1.py
@@ -0,0 +1,6 @@
+email_id=input("enter your email id:")
+user_name=email_id[:7]
+domain_name=email_id[8:]
+print("User name:",user_name)
+print("Domain name:",domain_name)
+
---
+++ Tuple programs/pro 2.py
@@ -0,0 +1,6 @@
+tu=(-20,-4,-7,6,4,8,3,-6)
+positive_no=[]
+for i in tu:
+    if(i>0):
+        positive_no.append(i)
+print(tuple(positive_no))
